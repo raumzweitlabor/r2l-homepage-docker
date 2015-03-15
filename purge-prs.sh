955fd5ce83bd7c36491c6a10ac2c508f44e82a0a
Simon Elsbrock <simon@iodev.org>
2015-03-16 00:06:32 +0100

    Fix purge delete: don't delete .

--- purge-prs.sh
+++ purge-prs.sh
@@ -5,4 +5,4 @@ set -e
 DAYS_TO_KEEP=$(( 14 - 1))
 TARGET_DIR=/data/preview
 
-find $TARGET_DIR -maxdepth 1 -type d -mtime +$DAYS_TO_KEEP -exec rm -vrf {} \;
+find $TARGET_DIR/* -maxdepth 0 -type d -mtime +$DAYS_TO_KEEP -exec rm -vrf {} \;
